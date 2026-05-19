09095f5d94ee93ec3c2dcd2cbef977421b87777f
nicolascuellosuarez <jaimecuellosuarez@gmail.com>
2026-05-19 11:31:00 -0500

    FastApi App created, Backend to be made for app function.

---
+++ api/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+"""
+App initializer from original class FastAPI
+"""
+
+app = FastAPI(
+    title= "CultivaLab API",
+    description= "API para una app con el objetivo de "
+    "modelar y simular el crecimiento de cultivos",
+    version= "1.0.0"
+)
+
+"""
+CORS to allow connection from future FrontEnd
+"""
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"Message": "CultivaLab API working tree clean"}
+
+
+@app.get("/health")
+def health():
+    return {"Status": "Ok"}
