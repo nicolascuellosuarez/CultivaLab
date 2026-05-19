5a76dfa146556621d2701843d698fe5c711e162e
nicolascuellosuarez <jaimecuellosuarez@gmail.com>
2026-05-19 11:38:54 -0500

    Schemas of TypeHints made for authentication.

--- api/schemas/auth.py
+++ api/schemas/auth.py
@@ -1,2 +1,24 @@
 from pydantic import BaseModel, Field
 
+class LoginRequest(BaseModel):
+    username: str = Field(..., min_length = 4, max_length = 30)
+    password: str = Field(..., min_length = 8)
+
+class RegisterRequest(BaseModel):
+    username: str = Field(..., min_length = 4, max_length = 30)
+    password: str = Field(..., min_length = 8)
+
+class RegisterAdminRequest(BaseModel):
+    admin_key: str = Field(..., description = "Master Key for Admin Sign - In")
+
+class LoginResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    username: str
+    role: str
+
+class UserResponse(BaseModel):
+    id: str
+    username: str
+    role: str
+
